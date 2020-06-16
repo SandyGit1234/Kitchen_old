b9ab1a650a64f85bd764c4d87ee11ae6135d83e9
Sandygit1234 <sandy.shv1234@gmail.com>
2020-06-16 12:20:21 -0400

    Sandeep First Upload for Kitchen Website

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Sandy\PycharmProjects\django Projects\Kitchen\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
